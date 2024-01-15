2c1306e99246c93fe4edf53e13c42d2a8ff5a489
RUWAA ALI <Ruwaa@RUWAAs-MacBook-Air.local>
2024-01-15 16:46:18 +0300

    ápdated git commit -m ápdated

--- src/Design.jsx
+++ src/Design.jsx
@@ -11,19 +11,19 @@ function Designone({items}) {
     }
   return (
     <div  className='bg-green-]'>
-    <div className='bg-green-300  mt-[60px] w-[50%] h-[50px] ml-[350px] flex'>
+    <div className='bg-green-300  mt-[60px] w-[50%] h-[50px] sm:ml-[350px] ml-[30px] flex'>
         <h1 className ='p-4 text-2xl'></h1>
     <p className='font-bold text-2xl ml-4' >{items}</p> 
-    <i class="fa-solid fa-minus  text-3xl ml-[580px] mt-1 hidden"  onClick={handleminus} style={{display: open === true ? "block" : "none"}}></i>
-    <i  class="fa-solid fa-plus text-3xl ml-[580px] mt-1" onClick={handleplus}  style={{display: open === true ? "none" : "block"}} ></i>
+    <i class="fa-solid fa-minus  text-3xl sm:ml-[580px] ml-[150px] mt-1 hidden"  onClick={handleminus} style={{display: open === true ? "block" : "none"}}></i>
+    <i  class="fa-solid fa-plus text-3xl sm:ml-[580px] ml-[150px] mt-1" onClick={handleplus}  style={{display: open === true ? "none" : "block"}} ></i>
     </div>
   
     
-    <div style={{display: open === true ? "block" : "none"}} className='bg-white w-[50%] h-[160px] border-red-300 border-4  ml-[350px] text-xl text-black px-5 mt-4'>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, ullam!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, ullam!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, ullam!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, ullam!
+    <div style={{display: open === true ? "block" : "none"}} className='bg-white w-[50%] h-[180px] border-red-300 border-4 sm:ml-[350px] ml-[20px] sm:text-xl text-black sm:px-5 p-8 sm:mt-4'>
+      Lorem ipsum it amet consectetur, adipisicing elit. Atque, ullam!
+      Lorem ipsum , adipisicing elit. Atque, ullam!
+      Lorem ipsum dolor sit amet consectetur, a!
+      Lorem ipsum dolor sit amet consectetur,!
     </div>
     </div>
     
